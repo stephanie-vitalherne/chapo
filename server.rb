03338d5cfa87fc633a7fff41d21109307a824838
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-08-20 15:20:41 -0400

    getting the routes to work now

--- server.rb
+++ server.rb
@@ -11,3 +11,19 @@ end
 get '/signup' do
   erb :signup
 end
+
+get '/login' do
+  erb :login
+end
+
+get '/account' do
+  erb :account
+end
+
+get '/logout' do
+  session[:user] = nil
+  p 'user has logged out'
+  redirect '/'
+end
+
+require './models'
