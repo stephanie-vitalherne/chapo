0bb6c1daac2fe7cb5c2e1376404b54a03ad973db
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-08-20 23:09:28 -0400

    server done up to users. post will happen later

--- server.rb
+++ server.rb
@@ -1,10 +1,13 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+enable :sessions
 
 set :database, 'sqlite3:rumblr.sqlite3'
 
 get '/' do
   p 'Baana'
+  @users = User.all
+  p @users
   erb :home
 end
 
@@ -12,8 +15,33 @@ get '/signup' do
   erb :signup
 end
 
+post '/signup' do
+  user = User.new(
+    first_name: params['first_name'],
+    last_name: params['last_name'],
+    username: params['username'],
+    email: params['email'],
+    password: params['password']
+  )
+  user.save
+  redirect '/'
+end
+
 get '/login' do
   erb :login
+end
+
+post '/login' do
+  email = params['email']
+  given_password = params['password']
+  user = User.find_by(email: email)
+  if user.password == given_password
+    session[:user] = user
+    redirect :account
+  else
+    p 'Invalid credentials'
+    redirect '/'
+  end
 end
 
 get '/account' do
